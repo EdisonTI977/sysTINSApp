51c273ef835635ff68716823cdeec5e7bb9bc8ea
edison.epfernandes <edison.epfernandes@ITQ0675989W10-1>
2024-11-04 14:58:23 -0300

    criei os botoes do formulario

---
+++ atividade04.11.cs
@@ -0,0 +1,69 @@
+﻿namespace sysTINSApp
+{
+    public partial class Atividade04 : Form
+    {
+        public Atividade04()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Atividade04_Load(object sender, EventArgs e)
+        {
+            Atividade04 atividade04 = new()
+            {
+                MdiParent = this // associa esta instância ao container (MDI) FrmPrincipal
+            };
+            atividade04.Show(); // exibe o form Usuários na janela do sistema 
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
