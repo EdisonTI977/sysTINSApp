690d02b1df2a73ca23a8181e5f4e4e6059eda4a9
edison.epfernandes <edison.epfernandes@ITQ0675989W10-1>
2024-11-04 15:26:09 -0300

    tela de login email e senha

---
+++ Atividade email e senha.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sysTINSApp
+{
+    public partial class Atividade_email_e_senha : Form
+    {
+        public Atividade_email_e_senha()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
